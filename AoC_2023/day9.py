52b03d7df260869904321ecafe562ee1db6821b0
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-12-09 15:16:33 +0000

    Simplify day 9 code.

--- AoC_2023/day9.py
+++ AoC_2023/day9.py
@@ -23,44 +23,29 @@ def process_input(raw_lines):
     return s
 
 
-def next_val(s):
+def extend_seq(s):
     """
-    Recursively check diffs and store last val at each level, then sum for ans.
+    Recursively check diffs and store last/first val each time, then sum/subtract.
     """
     not_found = True
     d = s[:]
     last_val = [d[-1]]
-    while not_found:
-        d = list(diff(d))
-        last_val.append(d[-1])
-        if d.count(0) == len(d):
-            not_found = False
-
-    return sum(last_val)
-
-
-def prev_val(s):
-    """
-    Recursively check diffs and store first val at each level, then subtract for ans.
-    """
-    # Recursively check diffs and store first val in seq
-    not_found = True
-    d = s[:]
     first_val = [d[0]]
     while not_found:
         d = list(diff(d))
         first_val.append(d[0])
+        last_val.append(d[-1])
         if d.count(0) == len(d):
             not_found = False
 
     for i in range(len(first_val) - 1, 0, -1):
         first_val[i - 1] = first_val[i - 1] - first_val[i]
 
-    return first_val[0]
+    return sum(last_val), first_val[0]
 
 
 if __name__ == "__main__":
-    seqs = process_input(read_file("data/day9.dat"))
+    # seqs = process_input(read_file("data/day9.dat"))
     seqs = process_input(read_file("data/day9.test"))
 
     # print(seqs)
@@ -69,9 +54,9 @@ if __name__ == "__main__":
     ans_one = 0
     ans_two = 0
     for seq in seqs:
-        ans_one += next_val(seq)
-        ans_two += prev_val(seq)
+        d_1, d_2 = extend_seq(seq)
+        ans_one += d_1
+        ans_two += d_2
 
     print(f"Part one answer is {ans_one}")
-
     print(f"Part two answer is {ans_two}")
